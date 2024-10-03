7ea34a729fa131b23856926ae9a6150a75138198
frank gimeno <frankgimeno3@gmail.com>
2024-10-03 15:42:05 +0200

    resset password completado

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -15,8 +15,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-full flex-none md:w-64">
 
 
-        return (
-        <div className="flex h-full flex-col px-3 py-4 md:px-2">
+         <div className="flex h-full flex-col px-3 py-4 md:px-2">
           <Link
             className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
             href="/"
